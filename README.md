f04754fb27954cba0edd787ce0fdf961e2030175
Jackson Smith <jacksonsmith@Jacksons-MacBook-Air-2.local>
2023-05-25 18:21:18 -0500

    clickup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+## Ultimate Goal
 Once tagged in shipstation then the info is transferred to Click-Up
+
+## Goal for Completion
+Pulls data from ShipStation and adds it to CLickUp by creating a task and filling in the right fields for easy use. Eliminates the use for switching back and forth from ShipStation to ClickUp.
--- ShipStation/Node/node.js
+++ ShipStation/Node/node.js
@@ -1,10 +1,10 @@
 // let userNamePassword = '1d392e5455244baa81a92b777840a6db:952f798ead8c4141bd9f2691b11e3fed';
-let newNamePassword = 'MWQzOTJlNTQ1NTI0NGJhYTgxYTkyYjc3Nzg0MGE2ZGI6OTUyZjc5OGVhZDhjNDE0MWJkOWYyNjkxYjExZTNmZWQ='
-let auth = `Basic ${newNamePassword}`;
+const newNamePassword = 'MWQzOTJlNTQ1NTI0NGJhYTgxYTkyYjc3Nzg0MGE2ZGI6OTUyZjc5OGVhZDhjNDE0MWJkOWYyNjkxYjExZTNmZWQ='
+const auth = `Basic ${newNamePassword}`;
 const request = require('request');
 const inquirer = require('inquirer');
 
-
+// This function asks the user a question on who they are looking for, and then adds that name to the search to pull the customerid for the function dataPull()
 const freightShipment = async () => {
     try {
         const questions = await inquirer.prompt([
@@ -28,7 +28,7 @@ const freightShipment = async () => {
             if (error) throw new Error(error);
             let responseBody = JSON.parse(response.body);
             let custId = responseBody.orders[0].customerId;
-            console.log(custId);
+            // console.log(custId);
             dataPull(custId);
         });
 
@@ -39,6 +39,7 @@ const freightShipment = async () => {
 
 freightShipment();
 
+// This functions below pulls the Address using the customerid from the above function
 async function dataPull(custId) {
     let info = {
         'method': 'GET',
@@ -52,16 +53,14 @@ async function dataPull(custId) {
     request(info, function (error, response) { 
         if (error) throw new Error(error);
         let parseBody = JSON.parse(response.body);
+        let dateOrdered = parseBody.createDate;
+        let nameOfCustomer = parseBody.name;
+        let addressOfCustomer = parseBody.street1 + ',' + parseBody.street2 + ' ' + parseBody.city + ', ' + parseBody.state + ' ' +parseBody.postalCode + ' ' + parseBody.countryCode;
+        let marketplaceWeb = parseBody.marketplaceUsernames[0].marketplace;
+        let phoneNumber = parseBody.phone;
+        let emailOfCustomer = parseBody.email;
         console.log(parseBody);
     });
 }
 
-
-// const moveToClickUp = async () => {
-//     try {
-
-//     } catch (err) {
-//         console.log(err);
-//     }
-// }
-
+// module.exports = { dataPull };
